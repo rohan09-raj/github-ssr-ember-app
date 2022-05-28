92364fbbdae588f31a0ee3fd5a3acbf23d4ae0c3
rohan09-raj <rajrohan1914@gmail.com>
2022-05-28 13:08:38 +0530

    modified fastboot config file

--- fastboot-server.js
+++ fastboot-server.js
@@ -5,6 +5,8 @@ const MY_GLOBAL = 'MY GLOBAL';
 let server = new FastBootAppServer({
   distPath: 'dist',
   gzip: true, // Optional - Enables gzip compression.
+  host: '0.0.0.0', // Optional - Sets the host the server listens on.
+  port: 4000, // Optional - Sets the port the server listens on (defaults to the PORT env var or 3000).
   buildSandboxGlobals(defaultGlobals) {
     // Optional - Make values available to the Ember app running in the FastBoot server, e.g. "MY_GLOBAL" will be available as "GLOBAL_VALUE"
     return Object.assign({}, defaultGlobals, { GLOBAL_VALUE: MY_GLOBAL });
